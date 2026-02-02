af28adb45e3f9650838c57c146573d6f68a10327
josuelimited <geraldo7888@gmail.com>
2026-02-02 17:45:39 -0500

    asi finalizada la tarea

---
+++ app/app.js
@@ -0,0 +1,37 @@
+// Solicitud 1: Realice una solicitud GETpara obtener la lista completa de usuarios disponibles
+// en el servicio.
+
+// const solicitudGET = async ()=>{
+//     const reponse = await fetch(`http://localhost:3000/users`)
+//     const data = await reponse.json()
+//     console.log(data);
+// }
+// solicitud();
+
+
+
+
+
+// Solicitud 2: Realice una solicitud GET para consultar la información de un usuario
+// específico, utilizando su identificador.
+
+// const solicitudGET = async (id) =>{
+//   const reponse = await fetch(`http://localhost:3000/users/${id}`)
+//   const data = await reponse.json()
+//   console.log(data);
+// }
+// solicitudGET(2);
+
+
+
+
+
+// Solicitud 3: Realice una solicitud GET para obtener todas las publicaciones (posts)
+// asociadas a un usuario determinado.
+
+// const solicitudGET = async (id) =>{
+//   const reponse = await fetch(`http://localhost:3000/posts/${id}`)
+//   const data = await reponse.json()
+//   console.log(data);
+// }
+// solicitudGET(2);
---
+++ app/app2.js
@@ -0,0 +1,44 @@
+// Solicitud 4: Realice una solicitud POST para crear una nueva publicación asociada a un
+// usuario existente.
+// Incluya información como título y contenido.
+
+// const solicitudPOST = async (id)=>{
+// fetch(`http://localhost:3000/users`,{
+//     method:'POST',
+//     body: JSON.stringify({
+//     title: `Tambien caeras`,
+//     body: `bromas pesadas`
+//     }),
+//     headers:{
+//     'Content-type': 'aplication/json; charset=UTF-8',
+//     },
+// })
+//  .then((response) => response.json())
+//  .then((json)=> console.log(json));
+// }
+
+// solicitudPOST(2);
+
+
+
+
+// Solicitud 5: Realice una solicitud POST para registrar un nuevo comentario relacionado con
+// una publicación.
+
+
+// const solicitudPOST = async (id)=>{
+// fetch(`http://localhost:3000/comments`,{
+//     method:'POST',
+//     body: JSON.stringify({
+//     postId: id,
+//     body: `el clima esta muy friooo`
+//     }),
+//     headers:{
+//     'Content-type': 'aplication/json; charset=UTF-8',
+//     },
+// })
+//  .then((response) => response.json())
+//  .then((json)=> console.log(json));
+// }
+
+// solicitudPOST(11);
